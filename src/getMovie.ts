0e221079695a46debe27d38a51cae3a2322edd31
victor zubiaga b <vico.music81@outlook.com>
2024-11-05 23:03:21 -0500

    in progress

--- src/getMovie.ts
+++ src/getMovie.ts
@@ -3,6 +3,6 @@ import { APIGatewayProxyHandler } from "aws-lambda";
 export const handler: APIGatewayProxyHandler = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ name: "victor" }),
+    body: JSON.stringify({ name: "victor z", version: "2" }),
   };
 };
